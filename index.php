8be5598eb926d060575eb46fa70c4837d989bcdf
muhannas1812-star <muhannas1812@gmail.com>
2026-06-25 15:21:53 +0900

    menambahkan paragraph

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Praktik Git Remote 2</title>
+</head>
+<body>
+  <h1>Hello World</h1>
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente debitis minus hic? Asperiores temporibus ullam velit provident, nihil rem vitae. Maxime nisi tempore commodi esse placeat beatae harum aliquid veritatis.
+</body>
+</html>
